57e3fa7516fa6190fc9dc7e71a81816b3a56bc05
Naman-Bhrgv <62867828+Naman-Bhrgv@users.noreply.github.com>
2023-12-01 10:01:33 -0500

    Update exp_main.py

--- exp_main.py
+++ exp_main.py
@@ -45,7 +45,7 @@ if __name__ == '__main__':
 
             # Evaluation function for CGP (training CNN and return validation accuracy)
             eval_f = CNNEvaluation(gpu_num=2, dataset='cifar10', valid_data_ratio=0.1, verbose=True,
-                                epoch_num=5, batchsize=128)
+                                epoch_num=10, batchsize=128)
 
             # Execute evolution
             print("Evolution started.")
